34a1f410cd1711b02e1466474f1e72ffc649cb47
Sebastian Joseph <SJoseph@ucvts.tec.nj.us>
2021-10-22 14:09:14 -0400

    products

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        _id:'1',
+        name: 'Salatin Apples',
+        image: 'src/images/apple.png',
+        description:
+        'Apples farmed and picked by the Legendary Farmer Joel Salatin. Carries the essence of the gods, the nectar of the titans and the juice of man',
+        brand: 'Seb Farms',
+        category: 'Fruits',
+        price: 99.99,
+        countInStock: 5,
+        rating: 5, 
+        numReviews: 92,
+
+    },
+    {
+        _id: '2',
+        name: 'Salatin Bananas'
+    }
+]
